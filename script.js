011d931a8ec33d2f2fa9f580119c223b6269dbf5
ayush katiya <112498869+AyushKatiya20322@users.noreply.github.com>
2025-09-15 17:34:50 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+// Smooth scroll for nav links
+document.addEventListener('DOMContentLoaded', function () {
+    document.querySelectorAll('a.nav-link').forEach(link => {
+        link.addEventListener('click', function (e) {
+            if (this.hash) {
+                e.preventDefault();
+                document.querySelector(this.hash).scrollIntoView({
+                    behavior: 'smooth'
+                });
+            }
+        });
+    });
+});
